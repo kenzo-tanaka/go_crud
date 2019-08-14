67ada96fe30eb2d2b985e13dbaa9439d042fba0a
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-08-14 21:43:27 +0900

    add: dbinit & dbGetAll

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
     "github.com/gin-gonic/gin"
+    "github.com/jinzhu/gorm"
+
+    _ "github.com/mattn/go-sqlite3"
 )
 
 type Book struct {
@@ -9,16 +12,39 @@ type Book struct {
     Description string
 }
 
+func dbInit() {
+    db, err := gorm.Open("sqlite3", "book.sqlite3")
+    if err != nil {
+        panic("You can't open db! (init)")
+    }
+
+    defer db.Close()
+    db.AutoMigrate(&Book{})
+}
+
+func dbGetAll() []Book {
+    db, err := gorm.Open("sqlite3", "book.sqlite3")
+    if err != nil {
+        panic("You can't open DB (dbGetAll())")
+    }
+    defer db.Close()
+    var books []Book
+    db.Order("created_at desc").Find(&books)
+    return books
+}
+
 func main() {
     router := gin.Default()
     router.LoadHTMLGlob("templates/*.html")
 
-    data := "Go/Gin!!"
+    dbInit()
     
     router.GET("/", func(ctx *gin.Context){
+        books := dbGetAll()
         ctx.HTML(200, "index.html", gin.H{
-            "data": data,
-            "book": []Book{{"Sample book", "Lorem ipsum dolor..."}, {"Sample book2", "Lorem ipsum ..."}},
+            // "data": data,
+            // "book": []Book{{"Sample book", "Lorem ipsum dolor..."}, {"Sample book2", "Lorem ipsum ..."}},
+            "books": books,
         })
     })
 
