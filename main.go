695b31e0fd5a9860f65787d4ea6e7e8f807d54b8
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-08-14 21:46:21 +0900

    整形: main.go

--- main.go
+++ main.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/jinzhu/gorm"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 
-    _ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Book struct {
-    Title string
-    Description string
+	Title       string
+	Description string
 }
 
 func dbInit() {
-    db, err := gorm.Open("sqlite3", "book.sqlite3")
-    if err != nil {
-        panic("You can't open db! (init)")
-    }
+	db, err := gorm.Open("sqlite3", "book.sqlite3")
+	if err != nil {
+		panic("You can't open db! (init)")
+	}
 
-    defer db.Close()
-    db.AutoMigrate(&Book{})
+	defer db.Close()
+	db.AutoMigrate(&Book{})
 }
 
 func dbGetAll() []Book {
-    db, err := gorm.Open("sqlite3", "book.sqlite3")
-    if err != nil {
-        panic("You can't open DB (dbGetAll())")
-    }
-    defer db.Close()
-    var books []Book
-    db.Order("created_at desc").Find(&books)
-    return books
+	db, err := gorm.Open("sqlite3", "book.sqlite3")
+	if err != nil {
+		panic("You can't open DB (dbGetAll())")
+	}
+	defer db.Close()
+	var books []Book
+	db.Order("created_at desc").Find(&books)
+	return books
 }
 
 func main() {
-    router := gin.Default()
-    router.LoadHTMLGlob("templates/*.html")
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
 
-    dbInit()
-    
-    router.GET("/", func(ctx *gin.Context){
-        books := dbGetAll()
-        ctx.HTML(200, "index.html", gin.H{
-            // "data": data,
-            // "book": []Book{{"Sample book", "Lorem ipsum dolor..."}, {"Sample book2", "Lorem ipsum ..."}},
-            "books": books,
-        })
-    })
+	dbInit()
 
-    router.Run()
+	router.GET("/", func(ctx *gin.Context) {
+		books := dbGetAll()
+		ctx.HTML(200, "index.html", gin.H{
+			// "data": data,
+			// "book": []Book{{"Sample book", "Lorem ipsum dolor..."}, {"Sample book2", "Lorem ipsum ..."}},
+			"books": books,
+		})
+	})
+
+	router.Run()
 }
