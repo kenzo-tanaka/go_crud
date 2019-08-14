3dd92d862ff591b7728055569d652a13709e4da3
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-08-14 21:24:28 +0900

    fix: main.go (リスト一覧表示させる)

--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import (
     "github.com/gin-gonic/gin"
 )
 
+type Book struct {
+    Title string
+    Description string
+}
+
 func main() {
     router := gin.Default()
     router.LoadHTMLGlob("templates/*.html")
@@ -11,7 +16,10 @@ func main() {
     data := "Go/Gin!!"
     
     router.GET("/", func(ctx *gin.Context){
-        ctx.HTML(200, "index.html", gin.H{"data": data})
+        ctx.HTML(200, "index.html", gin.H{
+            "data": data,
+            "book": []Book{{"Sample book", "Lorem ipsum dolor..."}, {"Sample book2", "Lorem ipsum ..."}},
+        })
     })
 
     router.Run()
